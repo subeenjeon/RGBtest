a12360c307f3e1a1b8b51a72a001b42ba62877cf
subeenjeon <powersubeen@naver.com>
2014-12-04 14:30:55 +0900

    fg
    gfg

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var rSlider, gSlider, bSlider;
+
+function setup() {
+  // create canvas
+  createCanvas(710, 400);
+  textSize(15)
+  noStroke();
+
+  // create sliders
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(20, 20);
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(20, 50);
+  bSlider = createSlider(0, 255, 255);
+  bSlider.position(20, 80);
+}
+
+function draw() {
+  var r = rSlider.value();
+  var g = gSlider.value();
+  var b = bSlider.value();
+  background(r, g, b);
+  text("red", 165, 35);
+  text("green", 165, 65);
+  text("blue", 165, 95);
+}
